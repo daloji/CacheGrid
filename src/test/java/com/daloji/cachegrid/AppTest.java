a5a0a57cd9e147de226cd18211d165c65c6f7a24
daloji <jiantila@gmail.com>
2020-03-28 12:19:50 +0100

    modify settings

--- src/test/java/com/daloji/cachegrid/AppTest.java
+++ src/test/java/com/daloji/cachegrid/AppTest.java
@@ -1,38 +1,52 @@
 package com.daloji.cachegrid;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+
+import org.junit.Test;
+
+import com.daloji.caching.data.CacheSettings;
+import com.daloji.caching.data.Engine;
+import com.daloji.caching.data.ServerCaches;
+
+
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest  {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+    
+    @Test
+    public void test001() throws JAXBException {
+    	
+    	
+    	ServerCaches serv = new ServerCaches();
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    	CacheSettings cache = new CacheSettings();
+    	cache.setEngine(Engine.REDIS);
+    	cache.setName("rediscache");
+    	cache.setIpAdresse("127.0.0.1");
+    	cache.setPort("8364");
+    	List<CacheSettings> listcache = new ArrayList<CacheSettings>();
+    	listcache.add(cache);
+    	listcache.add(cache);
+    	serv.getCacheSettings().addAll(listcache);
+    
+    	
+    	File file = new File("C:\\Users\\A632436\\file.xml");
+		JAXBContext jaxbContext = JAXBContext.newInstance(ServerCaches.class);
+		Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+
+		// output pretty printed
+		jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+		jaxbMarshaller.marshal(serv, file);
+		jaxbMarshaller.marshal(serv, System.out);
     }
 }
